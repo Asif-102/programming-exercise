00c38029d28b685a51752eaedceaa1ae5e1f6988
Asif <58527783+Asif-102@users.noreply.github.com>
2022-05-29 19:07:05 +0600

    Update days_count.c
    organize code using struct with function

--- days_count.c
+++ days_count.c
@@ -9,46 +9,58 @@ struct Date
 
 int daysInMonths[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
+void isLeapYear(struct Date date)
+{
+    if ((date.year % 4 == 0 && date.year % 100 != 0) || date.year % 400 == 0)
+    {
+        daysInMonths[1] = 29;
+    }
+    else
+    {
+        daysInMonths[1] = 28;
+    }
+}
+
+struct Date getNextDate(struct Date date)
+{
+    isLeapYear(date);
+    if(date.day != daysInMonths[date.month -1])
+    {
+        date.day++;
+    }
+    else if(date.month != 12)
+    {
+        date.day = 1;
+        date.month++;
+    }
+    else
+    {
+        date.day = 1;
+        date.month = 1;
+        date.year++;
+    }
+    return date;
+}
+
+void printDate(struct Date date)
+{
+    printf("Next date: %d-%d-%d\n", date.day, date.month, date.year);
+}
+
 int main(void)
 {
     struct Date start;
-    int days;
-
-    printf("Start Date: ");
+    printf("Start days: ");
     scanf("%d %d %d", &start.day, &start.month, &start.year);
 
-    printf("How many Days: ");
+    int days;
+    printf("How many days you want to know: ");
     scanf("%d", &days);
 
     struct Date nextDate = start;
-
     for (int i = 0; i < days; i++)
     {
-        if((nextDate.year % 4 == 0 && nextDate.year % 100 != 0) || nextDate.year % 400 == 0)
-        {
-            daysInMonths[1] = 29;
-        }
-        else
-        {
-            daysInMonths[1] = 28;
-        }
-        
-        if (nextDate.day != daysInMonths[nextDate.month - 1])
-        {
-            nextDate.day++;
-        }
-        else if (nextDate.month != 12)
-        {
-            nextDate.day = 1;
-            nextDate.month++;
-        }
-        else
-        {
-            nextDate.day = 1;
-            nextDate.month = 1;
-            nextDate.year++;
-        }
-
-        printf("Next Date: %d-%d-%d\n", nextDate.day, nextDate.month, nextDate.year);
+        nextDate = getNextDate(nextDate);
+        printDate(nextDate);
     }
 }
